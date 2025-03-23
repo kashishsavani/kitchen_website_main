a2d3e0606d4a3ddd08f5b44820e1e31a0f792296
kashishsavani <savanikashish2@gmail.com>
2025-03-23 10:36:56 +0530

    add img

--- src/components/Product.js
+++ src/components/Product.js
@@ -55,7 +55,7 @@ const products = [
     Packing: "Packing with Ld Plastic Material or scratch Roll",
     MOQ: "6 Piece",
     imgSrc: "/img/All Size 28 6.png"
-  }
+  },
 ];
 
 const sizeOptions = [
