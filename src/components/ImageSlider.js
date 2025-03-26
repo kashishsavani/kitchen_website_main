52ab5f6346e90ecc10a95aef1a9c60015f691a3f
kashishsavani <savanikashish2@gmail.com>
2025-03-26 12:16:44 +0530

    image update

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -16,7 +16,17 @@ const ImageSlider = ({ images }) => {
   return (
     <div className="slider-container">
       <FaChevronLeft className="slider-btn left" onClick={prevSlide} />
-      <img src={images[currentIndex]} alt="Product" className="slider-img" />
+      {/* <img src={images[currentIndex]} alt="Product" className="slider-img" /> */}
+      <img
+  src={images[currentIndex]}
+  alt="Product"
+  className={`slider-img ${
+    (images[currentIndex] === "/img/5+44/1.jpg" || images[currentIndex] === "/img/5+44/4.jpg" || images[currentIndex] === "/img/3+15-1/4.jpg" || images[currentIndex] === "/img/3+20-1/4.jpg" || images[currentIndex] === "/img/3+25-1/4.jpg" || images[currentIndex] === "/img/3+30-1/4.jpg" || images[currentIndex] === "/img/4+36-1/4.jpg" || images[currentIndex] === "/img/5+44-1/4.jpg" || images[currentIndex] === "/img/41.jpg" || images[currentIndex] === "/img/5+39/4.jpg" || images[currentIndex] === "/img/4+39/4.jpg" || images[currentIndex] === "/img/4+33/4.jpg" || images[currentIndex] === "/img/4+27/4.jpg" || images[currentIndex] === "/img/6+52/4.jpg" || images[currentIndex] === "/img/6+42/3.jpg")
+      ? "large-img"
+      : ""
+  }`}
+/>
+
       <FaChevronRight className="slider-btn right" onClick={nextSlide} />
       <div className="slider-dots">
         {images.map((_, index) => (
